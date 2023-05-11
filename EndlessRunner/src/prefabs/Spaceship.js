06712b67045d615e3668b804681e3ae8046d4f1a
Adrian <keabruce@ucsc.edu>
2023-05-10 18:43:21 -0700

    working on the bat character

---
+++ EndlessRunner/src/prefabs/Spaceship.js
@@ -0,0 +1,32 @@
+// Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue, speed, fprefix) {
+      super(scene, x, y, texture, frame);
+  
+      // add object to existing scene
+      scene.add.existing(this);
+      this.points = pointValue;
+      this.moveSpeed = speed;
+      this.anims.create({
+        key: 'flap',
+        frameRate: 5,
+        repeat: -1,
+        frames: this.anims.generateFrameNames(texture,{prefix: fprefix, suffix: '.png', start:1, end: 2})
+      });
+    this.anims.play('flap');
+    }
+    freeze(){
+        this.anims.stop('flap');
+    }
+    update(){
+        //move spaceship left
+        this.x-=this.moveSpeed;
+        if(this.x<=0-this.width){
+            this.reset();
+        }
+    }
+
+    reset(){
+        this.x = game.config.width;
+    }
+}
