4d675a4b493918531dd16affc8c39d3f0c64082d
Adrian <keabruce@ucsc.edu>
2023-06-04 21:47:27 -0700

    moving buttons

--- SamuraiAndStrategy/src/scenes/Menu.js
+++ SamuraiAndStrategy/src/scenes/Menu.js
@@ -5,14 +5,14 @@ class Menu extends Phaser.Scene{
     preload(){
         //load audio
         this.load.image('menuBg', './assets/SamuraiGameMenu.png');
-        this.load.spritesheet('playButton', './assets/SamuraiGamePlay.png', {frameWidth: 203, frameHeight: 89});
-        this.load.spritesheet('credButton', './assets/SamuraiGameCredits.png', {frameWidth: 286, frameHeight: 108});
+        this.load.spritesheet('playButton', './assets/SamuraiGamePlay.png', {frameWidth: 203, frameHeight: 88});
+        this.load.spritesheet('credButton', './assets/SamuraiGameCredits.png', {frameWidth: 286, frameHeight: 107});
 
     }
     create() {
         this.add.sprite(game.config.width/2, game.config.height/2, 'menuBg').setOrigin(0.5, 0.5);
-        this.add.sprite(game.config.width*(7/8), game.config.height*(6/16), 'credButton').setOrigin(0.5, 0);
-        this.add.sprite(game.config.width*(7/8), game.config.height*(3/16), 'playButton').setOrigin(0.5, 0);
+        this.add.sprite(game.config.width*(13/16), game.config.height*(6/16), 'credButton').setOrigin(0, 0);
+        this.add.sprite(game.config.width*(13/16), game.config.height*(3/16), 'playButton').setOrigin(0, 0);
     }
     update(){
         
