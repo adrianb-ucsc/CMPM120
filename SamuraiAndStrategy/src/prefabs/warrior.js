1a9878a4d8887f5c3bacf75755e49a7ea59235a2
Adrian <keabruce@ucsc.edu>
2023-06-07 01:57:05 -0700

    added sfx

--- SamuraiAndStrategy/src/prefabs/warrior.js
+++ SamuraiAndStrategy/src/prefabs/warrior.js
@@ -6,23 +6,23 @@ class Warrior{
                 this.testRes = "This candidate isn't a samurai, they're a thief! They won't help, they'll steal the payment and run!"
                 break;
             case 1:
-                this.type = "Strat";
+                this.type = "Samurai(Strategist)";
                 this.testRes = "A samurai skilled in strategy. They will help ensure the village has strong defenses."
                 break;
             case 2:
-                this.type = "Sword";
+                this.type = "Samurai(Swordsman)";
                 this.testRes = "A samurai skilled in swordsmanship. They won't have any trouble killing bandits."
                 break;
             case 3:
-                this.type = "Teach";
+                this.type = "Samurai(Teacher)";
                 this.testRes = "This samurai seems to be a wise and disciplined teacher. They will have an easier time training the villagers."
                 break;
             case 4:
-                this.type = "Student";
+                this.type = "Samurai(Student)";
                 this.testRes = "This samurai is young and still training, but they may be able to help an experienced samurai work more quickly."
                 break;
             default:
-                this.type = "Merc";
+                this.type = "Mercenary";
                 this.testRes = "This warrior isn't a samurai, but they do have combat experience. Their help is probably easier to afford."
                 break;
         }
@@ -31,23 +31,23 @@ class Warrior{
         this.plan = 1;
         this.trainR = 1;
         this.cost = 10;
-        this.cost = Phaser.Math.Between(5, 10);
+        this.cost = Phaser.Math.Between(10, 15);
         this.studentMult = 1;
         this.recruitSuccess = true;
-        if(this.type == "Strat"){
+        if(this.type == "Samurai(Strategist)"){
             this.plan *= 2;
         }
-        if(this.type=="Sword"){
+        if(this.type=="Samurai(Swordsman)"){
             this.str *= 1.5;
             this.def *= 1.5;
         }
-        if(this.type=="Teach"){
+        if(this.type=="Samurai(Teacher)"){
             this.trainR = 2;
         }
-        if(this.type=="Student"){
+        if(this.type=="Samurai(Student)"){
             this.studentMult = 1.5;
         }
-        if(this.type=="Merc"){
+        if(this.type=="Mercenary"){
             this.str -= 1;
             this.def -= 1;
             this.cost /= 2;
@@ -60,8 +60,8 @@ class Warrior{
         return this.str;
     }
     test(){
-        if(this.type!="Merc"){
-            this.cost =Math.floor(this.cost*(Phaser.Math.FloatBetween(1.0, 2.0))) ;
+        if(this.type!="Mercenary"){
+            this.cost =Math.floor(this.cost*(Phaser.Math.FloatBetween(1.1, 2.0))) ;
         }
         return this.testRes; 
     }
@@ -90,23 +90,23 @@ class Warrior{
                 this.testRes = "This candidate isn't a samurai, they're a thief! They won't help, they'll steal the payment and run!"
                 break;
             case 1:
-                this.type = "Strat";
+                this.type = "Samurai(Strategist)";
                 this.testRes = "A samurai skilled in strategy. They will help ensure the village has strong defenses."
                 break;
             case 2:
-                this.type = "Sword";
+                this.type = "Samurai(Swordsman)";
                 this.testRes = "A samurai skilled in swordsmanship. They won't have any trouble killing bandits."
                 break;
             case 3:
-                this.type = "Teach";
+                this.type = "Samurai(Teacher)";
                 this.testRes = "This samurai seems to be a wise and disciplined teacher. They will have an easier time training the villagers."
                 break;
             case 4:
-                this.type = "Student";
-                this.testRes = "This samurai is young, and still learning, but they may be able to help a more experienced samurai work more efficiently."
+                this.type = "Samurai(Student)";
+                this.testRes = "This samurai is young, and still learning, but they could help an experienced samurai work more efficiently."
                 break;
             default:
-                this.type = "Merc";
+                this.type = "Mercenary";
                 this.testRes = "This warrior isn't a samurai, but they do have combat experience. Their help is probably easier to afford, too."
                 break;
         }
@@ -115,23 +115,23 @@ class Warrior{
         this.plan = 1;
         this.trainR = 1;
         this.cost = 10;
-        this.cost = Phaser.Math.Between(5, 10);
+        this.cost = Phaser.Math.Between(10, 15);
         this.studentMult = 1;
         this.recruitSuccess = true;
-        if(this.type == "Strat"){
+        if(this.type == "Samurai(Strategist)"){
             this.plan *= 2;
         }
-        if(this.type=="Sword"){
+        if(this.type=="Samurai(Swordsman)"){
             this.str *= 1.5;
             this.def *= 1.5;
         }
-        if(this.type=="Teach"){
+        if(this.type=="Samurai(Teacher)"){
             this.trainR = 2;
         }
-        if(this.type=="Student"){
+        if(this.type=="Samurai(Student)"){
             this.studentMult = 1.5;
         }
-        if(this.type=="Merc"){
+        if(this.type=="Mercenary"){
             this.str -= 1;
             this.def -= 1;
             this.cost /= 2;
--- SamuraiAndStrategy/src/scenes/Menu.js
+++ SamuraiAndStrategy/src/scenes/Menu.js
@@ -3,7 +3,7 @@ class Menu extends Phaser.Scene{
         super("menuScene");
     }
     preload(){
-        //load audio
+        this.load.audio('transition', './assets/transition.wav');
         this.load.image('menuBg', './assets/SamuraiGameMenu.png');
         this.load.spritesheet('playButton', './assets/SamuraiGamePlay.png', {frameWidth: 203, frameHeight: 88});
         this.load.spritesheet('credButton', './assets/SamuraiGameCredits.png', {frameWidth: 286, frameHeight: 107});
@@ -15,7 +15,9 @@ class Menu extends Phaser.Scene{
         this.add.sprite(game.config.width/2, game.config.height/2, 'menuBg').setOrigin(0.5, 0.5);
         this.clickCred = new Buttons(this, game.config.width*(12/16), game.config.height*(6/16), 'credButton', 0, {}, {}).setOrigin(0, 1);
         function playEffect(x){
+            x.sound.play('transition');
             x.scene.start('playScene');
+            
         }
         this.clickPlay = new Buttons(this, game.config.width*(12/16), game.config.height*(3/16), 'playButton', 0, playEffect).setOrigin(0, 1);
     
